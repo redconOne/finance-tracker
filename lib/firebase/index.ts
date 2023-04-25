e20c19d77f89f43fed3968ace0c7ce94dc4596c9
redconOne <mingleeng@gmail.com>
2023-04-25 08:22:26 -0600

    add: added firestore to app

---
+++ lib/firebase/index.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+require('dotenv').config();
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const API_KEY = process.env.API_KEY;
+const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
+const PROJECT_ID = process.env.PROJECT_ID;
+const STORAGE_BUCKET = process.env.STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.MESSAGING_SENDER_ID;
+const APP_ID = process.env.APP_ID;
+const MEASUREMENT_ID = process.env.MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { app, db, analytics };
