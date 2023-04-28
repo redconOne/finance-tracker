cdd710dcb24b286bcc5596190856bb4bfb1e3eac
redconOne <mingleeng@gmail.com>
2023-04-28 10:07:26 -0600

    add: added a viewExpenseModal to every expenseItem

--- components/ExpenseItem.tsx
+++ components/ExpenseItem.tsx
@@ -1,25 +1,48 @@
 import { currencyFormatter } from '@/lib/utils';
+import ViewExpenseModal from './modals/ViewExpenseModal';
+import { useState } from 'react';
 
-interface ExpenseItem {
-  title: string;
+type ExpenseItemProp = {
+  expense: ExpenseEntry;
+};
+
+type ExpenseEntry = {
+  id: string;
   color: string;
+  title: string;
   total: number;
-}
+  items: ExpenseEntryItem[];
+};
 
-const ExpenseItem = ({ title, color, total }: ExpenseItem) => {
+type ExpenseEntryItem = {
+  amount: number;
+  createdAt: Date;
+  id: string;
+};
+
+const ExpenseItem = ({ expense }: ExpenseItemProp) => {
+  const [showViewExpenseModal, setShowViewExpenseModal] = useState(false);
+
   return (
-    <button>
-      <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
-        <div className="flex items-center gap-2">
-          <div
-            className="rounded-full w-[25px] h-[25px]"
-            style={{ backgroundColor: color }}
-          />
-          <h4 className="capitalize">{title}</h4>
+    <>
+      <ViewExpenseModal
+        show={showViewExpenseModal}
+        onClose={setShowViewExpenseModal}
+        expense={expense}
+      />
+      <button onClick={() => setShowViewExpenseModal(true)}>
+        <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+          <div className="flex items-center gap-2">
+            <div
+              className="rounded-full w-[25px] h-[25px]"
+              style={{ backgroundColor: expense.color }}
+            />
+            <h4 className="capitalize">{expense.title}</h4>
+          </div>
+          <p>{currencyFormatter(expense.total)}</p>
         </div>
-        <p>{currencyFormatter(total)}</p>
-      </div>
-    </button>
+      </button>
+    </>
   );
 };
 
