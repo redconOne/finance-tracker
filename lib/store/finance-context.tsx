37c033cd3188557b9b051963c34808565b23798c
redconOne <mingleeng@gmail.com>
2023-04-27 18:48:26 -0600

    add: included expenses in context

--- lib/store/finance-context.tsx
+++ lib/store/finance-context.tsx
@@ -10,9 +10,9 @@ import {
   deleteDoc,
 } from 'firebase/firestore';
 
-interface Props {
+type Props = {
   children: React.ReactNode;
-}
+};
 
 type IncomeEntry = {
   id: string;
@@ -21,20 +21,37 @@ type IncomeEntry = {
   description: string | undefined;
 };
 
+type ExpenseEntryItem = {
+  amount: number;
+  createdAt: Date;
+  id: string;
+};
+
+type ExpenseEntry = {
+  id: string;
+  color: string;
+  title: string;
+  total: number;
+  items: ExpenseEntryItem[];
+};
+
 type financeContextType = {
   income: any;
+  expenses: any;
   addIncomeItem: Function;
   removeIncomeItem: Function;
 };
 
 export const financeContext = createContext<financeContextType>({
   income: [],
+  expenses: [],
   addIncomeItem: async () => {},
   removeIncomeItem: async () => {},
 });
 
 export default function FinanceContextProvider({ children }: Props) {
   const [income, setIncome] = useState<IncomeEntry[]>([]);
+  const [expenses, setExpenses] = useState<ExpenseEntry[]>([]);
 
   const addIncomeItem = async (newIncome: IncomeEntry) => {
     const collectionRef = collection(db, 'income');
@@ -71,7 +88,7 @@ export default function FinanceContextProvider({ children }: Props) {
     }
   };
 
-  const values = { income, addIncomeItem, removeIncomeItem };
+  const values = { income, expenses, addIncomeItem, removeIncomeItem };
 
   useEffect(() => {
     const getIncomedata = async () => {
@@ -86,11 +103,27 @@ export default function FinanceContextProvider({ children }: Props) {
           createdAt: new Date(doc.data().createdAt.toMillis()),
         };
       });
-
       setIncome(data);
     };
 
+    const getExpensesData = async () => {
+      const collectionRef = collection(db, 'expenses');
+      const docSnapshot = await getDocs(collectionRef);
+
+      const data = docSnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          color: doc.data().color,
+          title: doc.data().title,
+          total: doc.data().total,
+          items: doc.data().items,
+        };
+      });
+      setExpenses(data);
+    };
+
     getIncomedata();
+    getExpensesData();
   }, []);
 
   return (
