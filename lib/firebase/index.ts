7baf0e9f6c783967ba50150146df6db29edc830e
redconOne <mingleeng@gmail.com>
2023-05-08 14:05:41 -0600

    add: implemented firebase authentication

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from 'firebase/auth';
+// import { getAnalytics } from 'firebase/analytics';
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
@@ -30,6 +24,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 // const analytics = getAnalytics(app);
 
-export { app, db };
+export { app, db, auth };
